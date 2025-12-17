9a3e61b4b2a3791a3df041af901f36ce33eda5fb
Nikita Rodionov <proud9proud@gmail.com>
2025-12-18 02:38:31 +0300

    Remove redundant code

--- packages/walletkit/src/utils/address.ts
+++ packages/walletkit/src/utils/address.ts
@@ -23,12 +23,7 @@ export function asAddressFriendly(data?: Address | string | null): UserFriendlyA
         return data.toString() as UserFriendlyAddress;
     }
     try {
-        if (data) {
-            if (Address.isFriendly(data)) {
-                return Address.parseFriendly(data).address.toString() as UserFriendlyAddress;
-            }
-            return Address.parse(data).toString() as UserFriendlyAddress;
-        }
+        if (data) return Address.parse(data).toString() as UserFriendlyAddress;
     } catch {
         /* empty */
     }
