d9bf65d2d7df774af1c49f1c0c4986754a06a145
VK <heyllog@gmail.com>
2026-05-25 23:48:19 +0400

    deps: delete storybook static and add it to gitignore

--- packages/appkit-react/storybook-static/assets/react-BTU3j5Af.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./DocsRenderer-LL677BLK-Cy1GugUi.js";t();export{e as MDXProvider};
--- packages/appkit-react/storybook-static/assets/utils-vrHliU4b.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{Bt as t,F as n,Vt as r}from"./iframe-CbKJG4TV.js";var i,a=e((()=>{n(),i=(e,n)=>r(t(e||`0`,Math.min(5,n||9)),n)}));export{a as n,i as t};
